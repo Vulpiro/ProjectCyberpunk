77f39be4d45e66c25816c93882dc64bfd5380e0b
Vulpiro <vulpiroo@gmail.com>
2023-08-07 13:05:26 +0200

    Battle Calculate Distance Optimalization

--- Cyberpunk_simulation/Battle.cs
+++ Cyberpunk_simulation/Battle.cs
@@ -10,42 +10,7 @@ namespace Cyberpunk_simulation
     {
         public static int CalculateDistance(int x1, int y1, int x2, int y2)
         {
-            double distance = 0;
-            double x = 0;
-            double y = 0;
-            if (x1 > x2)
-            {
-                x = x1 - x2;
-            }
-            else if (x1 < x2)
-            {
-                x = x2 - x1;
-            }
-
-            if (y1 > y2)
-            {
-                y = y1 - y2;
-            }
-            else if (y1 < y2)
-            {
-                y = y2 - y1;
-            }
-
-            if (x == 0)
-            {
-                distance = y;
-            }
-            else if (y == 0)
-            {
-                distance = x;
-            }
-            else
-            {
-                x = x * x;
-                y = y * y;
-                double temp = x + y;
-                distance = Math.Sqrt(temp);
-            }
+            double distance = Math.Sqrt(Math.Pow(Math.Abs(x1 - x2), 2) + Math.Pow(Math.Abs(y1 - y2), 2));
 
             Console.WriteLine("Dystans pomiedzy postaciami rowna sie {0}", Convert.ToInt32(distance));
             return Convert.ToInt32(distance);
@@ -58,9 +23,9 @@ namespace Cyberpunk_simulation
                 c.GetInitiative();
             }
             Character[] sortedCharaArr = characterArray.OrderByDescending(c => c.Initiative).ToArray();
-            
+
         }
 
-        
+
     }
 }
