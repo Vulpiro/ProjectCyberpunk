1208f125ac0b2178ff76f8fbdaa39475b4784bde
Vulpiro <vulpiroo@gmail.com>
2023-09-19 13:58:22 +0200

    ugh

---
+++ Cyberpunk_simulation/Skill.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cyberpunk_simulation
+{
+    class Skill
+    {
+        public string Name { get; set; }
+        public int Base { get; set; }
+        public int Level { get; set; }
+
+        public Skill(string name, int stat){
+            Name = name;
+            Level = 0;
+            Base = stat;
+        }
+
+        public void SetLevel(int lvl, int stat)
+        {
+            Level = lvl;
+            Base = stat + lvl;
+        }
+
+        public void LevelUP(int stat)
+        {
+            Level++;
+            Base = stat + Level;
+        }
+    }
+}
